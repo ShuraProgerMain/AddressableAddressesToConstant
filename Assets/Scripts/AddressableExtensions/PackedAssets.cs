56ccbace9be442f47d9ff86dbe14a2286e45cb23
ShuraProger <stupidshitcreate@gmail.com>
2024-01-25 14:46:51 +0300

    Remove 'ToTitleCase'

---
+++ Assets/Scripts/AddressableExtensions/PackedAssets.cs
@@ -0,0 +1,8 @@
+namespace AddressableExtensions
+{
+   internal class PackedAssets
+   {
+        public const string TriangleSomeFuckingSiht = "TriangleSomeFuckingSiht";
+        public const string NewMaterial = "New Material";
+   }
+}
